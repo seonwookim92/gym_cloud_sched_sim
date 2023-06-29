1fc3297fbcf789047b4e6525484c886ca7d1fcbd
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-06-28 23:54:00 -0600

    Fix install requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_cloud_sched_sim',
       version='0.0.1',
-      install_requires=['gym', 'kaggle-environments', 'numpy']
+      install_requires=['gym', 'PyQt5', 'matplotlib']
 )
