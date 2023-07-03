60516c2587b98c2c0849ab3ed4f0d1ffa042ba2a
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-07-02 19:52:08 -0600

    Fix entry_point

--- gym_cloud_sched_sim/__init__.py
+++ gym_cloud_sched_sim/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='CloudSchedSim-v0',
-    entry_point='gym_cloud_sched_sim.gym_cloud_sched_sim.envs:CloudSchedSimEnv',
+    entry_point='envs:CloudSchedSimEnv',
 )
